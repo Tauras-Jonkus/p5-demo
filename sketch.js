57a29ff8911561cfefe3eb2cb2e8fd2255a84784
Tauras-Jonkus <jonkus.tauras@gmail.com>
2026-03-21 11:31:08 +0200

    Circle

--- sketch.js
+++ sketch.js
@@ -9,5 +9,15 @@ function draw() {
   Math.floor(Math.random() * 255),
   Math.floor(Math.random() * 255),
   Math.floor(Math.random() * 255)
-  )
+  );
+
+  var cicle_path_radius = 100;
+  var cicle_path_centre_x = 200;
+  var cicle_path_centre_y = 200;
+  var time = millis() / 1000;
+
+  var cicle_path_centre_x = cicle_path_radius*cos(time) + cicle_path_centre_x;
+  var cicle_path_centre_y = cicle_path_radius*sin(time) + cicle_path_centre_y;
+
+  circle(cicle_path_centre_x, cicle_path_centre_y, 100)
 }
