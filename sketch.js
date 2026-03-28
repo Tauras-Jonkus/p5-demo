4838a560333106dff755a33c96a6b4c44dd73255
Tauras-Jonkus <jonkus.tauras@gmail.com>
2026-03-28 10:43:57 +0200

    Bouncable

--- sketch.js
+++ sketch.js
@@ -1,23 +1,12 @@
+var test = new Bouncable(100, 100, 50);
+
+
 function setup() {
   createCanvas(400, 400);
-
-  frameRate(2)
 }
 
 function draw() {
-  background(
-  Math.floor(Math.random() * 255),
-  Math.floor(Math.random() * 255),
-  Math.floor(Math.random() * 255)
-  );
+  background(220);
 
-  var cicle_path_radius = 100;
-  var cicle_path_centre_x = 200;
-  var cicle_path_centre_y = 200;
-  var time = millis() / 1000;
-
-  var cicle_path_centre_x = cicle_path_radius*cos(time) + cicle_path_centre_x;
-  var cicle_path_centre_y = cicle_path_radius*sin(time) + cicle_path_centre_y;
-
-  circle(cicle_path_centre_x, cicle_path_centre_y, 100)
+  test.draw();
 }
