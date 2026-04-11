7f980bcdb1b1939ff0ce51b8cd829a5266c78504
Tauras-Jonkus <jonkus.tauras@gmail.com>
2026-04-11 10:47:17 +0300

    Choose

--- sketch.js
+++ sketch.js
@@ -3,22 +3,11 @@ var bouncables = [];
 function setup() {
   createCanvas(400, 400);
 
-  for (var i = 0; i < random(20, 100); i++){
-    bouncables.push(new Bouncable(
-    color(random(0, 255), random(0, 255), random(0,255)),
-    random(50, 350),
-    random(50, 350),
-    random(15, 50),
-    random(0.3, 4),
-    random(0, 360),
-  ));
-  }
+  setupBoucables();
 }
 
 function draw() {
   background(0);
 
-  for (var i = 0; i < bouncables.length; i++){
-    bouncables[i].draw();
-  }
+  drawBouncables();
 }
